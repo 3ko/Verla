e1c4082b431f80bc5cf2496192abc76eb53eb3fd
Administrator <m@3ko.fr>
2014-07-07 23:31:45 +0200

    .

---
+++ src/verla/storage/lru/lru.go
@@ -0,0 +1,108 @@
+package lru
+
+import (
+	"container/list"
+	"sync"
+)
+
+type cacheValue struct {
+	key string
+	cnx string
+}
+
+// Just an estimate
+func (v *cacheValue) size() uint64 {
+	return uint64(len(string(v.key)) + len(v.cnx))
+}
+
+type LRU struct {
+	sync.Mutex
+
+	// The approximate size of the structure (doesn't include the overhead of the data structures; just the
+	// sum of the size of the stored documents).
+	Size uint64
+
+	capacity uint64
+	list     *list.List
+	table    map[string]*list.Element
+}
+
+// Create a new Cache with a maximum size of capacity bytes.
+func New(capacity uint64) *LRU {
+	return &LRU{
+		capacity: capacity,
+		list:     list.New(),
+		table:    make(map[string]*list.Element),
+	}
+}
+
+// Insert some {key, document} into the cache. Doesn't do anything if the key is already present.
+func (c *LRU) Insert(key string, conexionstring string) {
+	c.Lock()
+	defer c.Unlock()
+
+	_, ok := c.table[key]
+	if ok {
+		return
+	}
+	v := &cacheValue{key, conexionstring}
+	elt := c.list.PushFront(v)
+	c.table[key] = elt
+	c.Size += v.size()
+	c.trim()
+}
+
+// Get retrieves a value from the cache and returns the value and an indicator boolean to show whether it was
+// present.
+func (c *LRU) Get(key string) (conexionstring string, ok bool) {
+	c.Lock()
+	defer c.Unlock()
+
+	elt, ok := c.table[key]
+	if !ok {
+		return "", false
+	}
+	c.list.MoveToFront(elt)
+	return elt.Value.(*cacheValue).cnx, true
+}
+
+// If the key is present, move that document to the front of the list to show that it was most recently used.
+func (c *LRU) Update(key string) {
+	c.Lock()
+	defer c.Unlock()
+
+	elt, ok := c.table[key]
+	if !ok {
+		return
+	}
+	c.list.MoveToFront(elt)
+}
+
+// Delete the document indicated by the key, if it is present.
+func (c *LRU) Delete(key string) {
+	c.Lock()
+	defer c.Unlock()
+
+	elt, ok := c.table[key]
+	if !ok {
+		return
+	}
+	delete(c.table, key)
+	v := c.list.Remove(elt).(*cacheValue)
+	c.Size -= v.size()
+}
+
+// If the cache is over capacity, clear elements (starting at the end of the list) until it is back under
+// capacity. Note that this method is not threadsafe (it should only be called from other methods which
+// already hold the lock).
+func (c *LRU) trim() {
+	for c.Size > c.capacity {
+		elt := c.list.Back()
+		if elt == nil {
+			return
+		}
+		v := c.list.Remove(elt).(*cacheValue)
+		delete(c.table, v.key)
+		c.Size -= v.size()
+	}
+}
---
+++ src/verla/storage/storage.go
@@ -0,0 +1,70 @@
+package storage
+
+import (
+	"./lru"
+	goconf "github.com/akrennmair/goconf"
+	"log"
+	"os"
+)
+
+type driver interface {
+	Get(string) (string, bool)
+	Set(string, string) error
+	Delete(string) error
+}
+
+func driverFactory(config *goconf.ConfigFile) driver {
+	drivername, err := config.GetString("storage", "driver")
+	if err != nil {
+		log.Printf("driver not setup")
+		os.Exit(1)
+	}
+	switch drivername {
+	case "memcached":
+		return NewMemcached(config)
+	default:
+		log.Printf("driver %s not implemented", drivername)
+		os.Exit(1)
+	}
+	return nil
+
+}
+
+type Storage struct {
+	lru    *lru.LRU
+	config *goconf.ConfigFile
+	driver driver
+}
+
+func New(config *goconf.ConfigFile) *Storage {
+	return &Storage{
+		config: config,
+		lru:    lru.New(10000),
+		driver: driverFactory(config),
+	}
+}
+
+func (c *Storage) Get(key string) (conexionstring string, ok bool) {
+
+	// log.Printf("get addr from %s ", key)
+
+	conexionstring, r := c.lru.Get(key)
+	if r {
+		return conexionstring, true
+	} else {
+		conexionstring, r = c.driver.Get(key)
+		if r {
+			c.lru.Insert(key, conexionstring)
+		}
+		return conexionstring, r
+	}
+}
+
+func (c *Storage) Set(key string, conexionstring string) {
+	c.driver.Set(key, conexionstring)
+}
+
+func (c *Storage) Delete(key string) {
+	c.lru.Delete(key)
+	c.driver.Delete(key)
+}
